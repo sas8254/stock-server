34a7f98f1e0d69da70d62af25031aa74a7179585
sas8254 <shashibhushansingh11@gmail.com>
2023-11-07 16:18:16 +0530

    add get position route

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -340,4 +340,31 @@ exports.placeLimtOrderMCX = async (req, res) => {
   }
 };
 
+exports.getPositions = async (req, res) => {
+  const newInstance = instance();
+
+  try {
+    const user = await User.findById(req.params.id);
+    const api_key = user.brokerDetail.apiKey;
+    const access_token = user.brokerDetail.dailyAccessToken;
+
+    const response = await newInstance.get("/portfolio/positions", {
+      headers: {
+        "X-Kite-Version": process.env.KITE_VERSION,
+        Authorization: `token ${api_key}:${access_token}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    // console.log(response);
+    if (response) {
+      res.status(200).json({ response });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "An error occurred",
+      error,
+    });
+  }
+};
+
 
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post("/place-order-NSE", orderController.placeLimtOrderNSE);
 router.post("/place-order-MCX", orderController.placeLimtOrderMCX);
 router.post("/place-order-NFO", orderController.placeLimtOrderNFO);
+router.get("/get-positions/:id", orderController.getPositions);
 
 module.exports = router;
